f04cfdc96b38d6420b122e628f4703b30254ea52
Veronika Dodda <v.shengeliya@gmail.com>
2021-01-16 12:30:53 -0500

    Updates

---
+++ precache-manifest.32265d19c0bbf4d45d449e333c282244.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07119e2c6c362886718929202ec469be",
+    "url": "/Shopify-Challenge/index.html"
+  },
+  {
+    "revision": "8fa81ef7d064049698d5",
+    "url": "/Shopify-Challenge/static/css/2.1afe660f.chunk.css"
+  },
+  {
+    "revision": "2e807c4b7b8ba119b7a4",
+    "url": "/Shopify-Challenge/static/css/main.f4464663.chunk.css"
+  },
+  {
+    "revision": "8fa81ef7d064049698d5",
+    "url": "/Shopify-Challenge/static/js/2.6106f66b.chunk.js"
+  },
+  {
+    "revision": "378175797a7f42a9dd5444db82c707d9",
+    "url": "/Shopify-Challenge/static/js/2.6106f66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e807c4b7b8ba119b7a4",
+    "url": "/Shopify-Challenge/static/js/main.9063e387.chunk.js"
+  },
+  {
+    "revision": "3d91b94a6a69da21d304",
+    "url": "/Shopify-Challenge/static/js/runtime-main.b5d58105.js"
+  }
+]);
